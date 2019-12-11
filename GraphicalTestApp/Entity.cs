dff1aceb84da595298c9f2b0767e24de2724f6c4
Tyrese Samuel <3tyrese@gmail.com>
2019-12-11 00:22:20 -0600

    Add project files.

---
+++ GraphicalTestApp/Entity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GraphicalTestApp
+{
+    class Entity : Actor
+    {
+        private Vector3 _velocity = new Vector3();
+        private Vector3 _acceleration = new Vector3();
+
+        public float XVelocity
+        {
+            //## Implement velocity on the X axis ##//
+            get
+            {
+               return _velocity.x;
+            }
+            set
+            {
+                _velocity.x = value;
+            }
+        }
+        public float XAcceleration
+        {
+            //## Implement acceleration on the X axis ##//
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            } 
+        }
+        public float YVelocity
+        {
+            //## Implement velocity on the Y axis ##//
+            get
+            {
+                return _velocity.y;
+            }
+            set
+            {
+                _velocity.y = value;
+            }
+        }
+        public float YAcceleration
+        {
+            //## Implement acceleration on the Y axis ##//
+            get { return 0; }
+            set { }
+        }
+
+        //Creates an Entity at the specified coordinates
+        public Entity(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override void Update(float deltaTime)
+        {
+            //## Calculate velocity from acceleration ##//
+            //## Calculate position from velocity ##//
+            base.Update(deltaTime);
+        }
+    }
+}
---
+++ GraphicalTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(1280, 760, "Graphical Test Application");
+
+            Actor root = new Actor();
+            game.Root = root;
+
+            game.Run();
+        }
+    }
+}
